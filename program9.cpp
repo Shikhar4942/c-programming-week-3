f1e839161080bc427cb50b931c8fe154f26d0967
Shikhar4942 <143405323+Shikhar4942@users.noreply.github.com>
2023-11-04 20:32:08 +0530

    program9.cpp

--- program9.cpp
+++ program9.cpp
@@ -1,13 +1,17 @@
 #include<stdio.h>
 int main()
 {
-	int a=0,b=1,n,i;
+	int a=0,b=1,n,i,c;
 	printf("enter the term upto which you want the series");
 	scanf("%d",&n);
 	printf("01");
+	c=a+b;
 	for(i=0;i<n-2;i++)
 	{
-		a=b+a;
-		printf("%d",a);
+		printf("%d",c);
+		a=b;
+		b=c;
+		c=a+b;
+		
 	}
 }
